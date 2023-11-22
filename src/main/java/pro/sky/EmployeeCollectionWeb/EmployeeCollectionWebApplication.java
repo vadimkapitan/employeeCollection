8f9717fea2f99c16eb4468ae8da4d98999839f75
vadimKapitan <xxxcap13@gmail.com>
2023-11-22 22:41:25 +0300

    Start

---
+++ src/main/java/pro/sky/EmployeeCollectionWeb/EmployeeCollectionWebApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeCollectionWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCollectionWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCollectionWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/EmployeeCollectionWeb/EmployeeCollectionWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.EmployeeCollectionWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCollectionWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
