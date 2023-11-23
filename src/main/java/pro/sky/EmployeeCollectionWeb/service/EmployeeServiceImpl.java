1a1fbf5078dac6721ac0de41f4adae1d2b556f7f
vadimKapitan <xxxcap13@gmail.com>
2023-11-23 12:18:27 +0300

    Home Work refactor 2.7 is done

--- src/main/java/pro/sky/EmployeeCollectionWeb/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/EmployeeCollectionWeb/service/EmployeeServiceImpl.java
@@ -5,44 +5,42 @@ import pro.sky.EmployeeCollectionWeb.exception.EmployeeAlreadyAddedException;
 import pro.sky.EmployeeCollectionWeb.exception.EmployeeNotFoundException;
 import pro.sky.EmployeeCollectionWeb.model.Employee;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private final List<Employee> employees = new ArrayList<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     @Override
     public Employee add(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (employees.contains(employee)) {
+        if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
         }
-        employees.add(employee);
+        employees.put(employee.getFullName(), employee);
         return employee;
     }
 
     @Override
     public Employee remove(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (!employees.contains(employee)) {
+        if (!employees.containsKey(employee.getFullName())) {
             throw new EmployeeNotFoundException();
         }
-        employees.remove(employee);
+        employees.remove(employee.getFullName());
         return employee;
     }
 
     @Override
     public Employee find(String firstName, String lastName) {
         Employee employee = new Employee(firstName, lastName);
-        if (!employees.contains(employee)) {
+        if (!employees.containsKey(employee.getFullName())) {
             throw new EmployeeNotFoundException();
         }
         return employee;
     }
     @Override
      public Collection<Employee> findAll() {
-        return employees;
+        return employees.values();
     }
 }
